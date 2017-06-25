f3d150d0d7ee36cdeda0b5832dd2dc70b6460a61
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-06-25 11:59:41 -0700

    Fixing issue with duplicate results for one click

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -21,36 +21,41 @@ function topicsIterator() {
         // console.log('text added', topicsButton);
 		$('#topics-buttons').append(topicsButton); //INSERT VARIABLE AT END OF THIS	
 		// console.log('end of button', topicsButton);
-	}
-}
+	}	
 
-$("#add-topic").on("click", function(event) {//may have an issue with teh jquery call
+	$("#add-topic").on("click", function(event) {//may have an issue with teh jquery call
         event.preventDefault();
 
 		// This line will grab the text from the input box
-        var userTopicText = $("#topic-input").val();
+        var userTopicText = $("#topic-input").val().trim();
 
         // The movie from the textbox is then added to our array
         topics.push(userTopicText);
         userTopicText = $("#topic-input").val("");
 
-        topicsIterator();
-});
+		topicsIterator();
+	});
+}
 
+//WORK ON API AJAX AND JSON
+function displayResults() {
+	var queryTerm = $(this).attr('data-name');
+	var apiKey = 'd16a7ac4a7da4915a463d555380fe185'; //CHANGE THE API KEY
+	var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + queryTerm + '&api_key=' + apiKey + '&limit=10'; //FIX THE LINK AND NAME
 
-// $("#add-movie").on("click", function(event) {
-// 		var queryTerm = topics[i];
-// 		var apiKey = 'dc6zaTOxFJmzC'; //change the api key
-// 		var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + queryTerm + '&api_key=' + apiKey + '&limit=10' + queryTerm[i]; //FIX THE LINK AND NAME
-		
-// 		$.ajax({
-// 	      url: queryURL,
-// 	      method: 'GET'
-// 	    }).done(function(response) {
-// 	      console.log(response);
+	$.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).done(function(response) {
+      console.log(queryTerm);
+      console.log(response);
 
-// 	    });
+      //add function to put stuff into the results
+
+    });
+}
 
 
 //FUNCTION CALLS
 topicsIterator();
+$(document).on("click", ".topics-styling", displayResults);
