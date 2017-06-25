4237c73d0ce6f5c79adbca219717c34b7294f55d
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-06-25 09:46:24 -0700

    Making javascript dynamically input buttons in array

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,2 +1,10 @@
-var topics = ['sci-fi', 'video games', 'life', 'motivation'];
+var topics = ['Super Mario', 'Call of Duty', 'Bomberman', 'Megaman'];
 
+function topicsIterator() {
+	$('#topics-buttons').empty();
+	for (var i = 0; i < topics.length; i++) {
+		$('#topics-buttons').append('<button type="button" class="btn btn-default">' + topics[i]); //INSERT VARIABLE AT END OF THIS
+	}
+}
+
+topicsIterator();
